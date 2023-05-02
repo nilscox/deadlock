9f7298712f20e04ed0489c3335844a358170dcad
nilscox <nilscox.dev@gmail.com>
2023-05-02 17:15:29 +0200

    initialize a vite project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '',
+  plugins: [react()],
   server: {
     port: 8000,
   },
