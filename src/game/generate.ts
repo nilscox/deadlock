3ef364a0d60453c2bbb8cfa6a864d099fc207d40
nilscox <nilscox.dev@gmail.com>
2023-05-04 06:00:45 +0200

    evaluate level difficulty

--- src/game/generate.ts
+++ src/game/generate.ts
@@ -1,8 +1,9 @@
-import { Cell, CellType } from './cell';
+import { CellType } from './cell';
 import { Direction, getDirectionVector } from './direction';
 import { Level, LevelDescription } from './level';
+import { randomTransformLevel } from './level-transforms';
 import { solve } from './solve';
-import { randBool, shuffle } from './utils';
+import { shuffle } from './utils';
 
 const emptyLevel = (width: number, height: number): Level => {
   const desc = Array(width * height)
@@ -22,38 +23,38 @@ const last = <T>(array: T[]) => {
   return array[array.length - 1];
 };
 
-const nextLevel = (cells: Cell[]) => {
-  const lastBlockIdx = cells.findLastIndex((cell) => cell.type === CellType.block);
+const nextLevel = (level: LevelDescription): number => {
+  const lastBlockIdx = level.findLastIndex((cell) => cell.type === CellType.block);
 
   if (lastBlockIdx === -1) {
     return -1;
   }
 
-  if (cells[lastBlockIdx] === last(cells)) {
-    const idx = nextLevel(cells.slice(0, -1));
+  if (level[lastBlockIdx] === last(level)) {
+    const idx = nextLevel(level.slice(0, -1));
 
-    if (idx === -1 || !cells[idx + 2]) {
+    if (idx === -1) {
       return -1;
     }
 
-    cells[lastBlockIdx].type = CellType.empty;
-    cells[idx + 2].type = CellType.block;
+    if (level[idx + 1]) {
+      level[lastBlockIdx].type = CellType.empty;
+      level[idx + 1].type = CellType.block;
+    }
   } else {
-    cells[lastBlockIdx].type = CellType.empty;
-    cells[lastBlockIdx + 1].type = CellType.block;
+    level[lastBlockIdx].type = CellType.empty;
+    level[lastBlockIdx + 1].type = CellType.block;
   }
 
-  return lastBlockIdx;
+  return lastBlockIdx + 1;
 };
 
 export const generateAllLevels = (width: number, height: number, blocks: number): LevelDescription[] => {
   const level = emptyLevel(width, height);
   const levels: LevelDescription[] = [];
 
-  const cells = level.emptyCells;
-
   for (let i = 0; i < blocks; ++i) {
-    cells[i].type = CellType.block;
+    level.cellsArray[i].type = CellType.block;
   }
 
   do {
@@ -66,25 +67,22 @@ export const generateAllLevels = (width: number, height: number, blocks: number)
   return levels;
 };
 
-export const isLevelRelevant = (level: LevelDescription) => {
-  const solutions = solve(level, 100);
+export const evaluateLevelDifficulty = (level: LevelDescription) => {
+  const solutions = solve(level);
 
   if (!solutions || solutions.length === 0) {
-    return false;
+    return -1;
   }
 
-  for (const solution of solutions) {
-    if (countJumps(solution) < 1) {
-      return false;
-    }
-  }
+  const numberOfSolutionsScore = Math.max(0, 10 - Math.floor(Math.log2(solutions.length)));
+  const simplestSolutionScore = solutions.map(evaluateSolutionSimplicity).sort((a, b) => a - b)[0];
 
-  return true;
+  return numberOfSolutionsScore + 3 * simplestSolutionScore;
 };
 
-const countJumps = (solution: Direction[]) => {
+const evaluateSolutionSimplicity = (solution: Direction[]) => {
   const cells = new Set<string>();
-  const jumps = new Set<string>();
+  let total = 0;
 
   let x = 0;
   let y = 0;
@@ -96,35 +94,51 @@ const countJumps = (solution: Direction[]) => {
   for (const [index, dir] of Object.entries(solution)) {
     const [dx, dy] = getDirectionVector(dir);
 
+    let jumped = 0;
+
     while (cells.has(key((x += dx), (y += dy)))) {
-      jumps.add(index);
+      jumped++;
+    }
+
+    if (jumped) {
+      total += jumped;
     }
 
     cells.add(key(x, y));
   }
 
-  return jumps.size;
+  return total;
 };
 
 export const generateLevels = (width: number, height: number, blocks: number) => {
-  const all = shuffle(generateAllLevels(width, height, blocks));
+  let levels = shuffle(generateAllLevels(width, height, blocks));
 
-  const solvable = all.filter((level, i) => {
-    console.log(`${i} / ${all.length} (${Math.floor((100 * i) / all.length)}%)`);
-    return isLevelRelevant(level);
-  });
+  const difficulties = new Map(
+    levels.map((level, i) => {
+      console.log(`${i} / ${levels.length} (${Math.floor((100 * i) / levels.length)}%)`);
+      return [level, evaluateLevelDifficulty(level)];
+    })
+  );
 
-  for (let i = 0; i < solvable.length; ++i) {
-    const level = new Level(solvable[i]);
+  const difficulty = (level: LevelDescription) => {
+    return difficulties.get(level) ?? 0;
+  };
+
+  levels = levels.filter((level) => difficulty(level) >= 0);
+  levels.sort((a, b) => difficulty(a) - difficulty(b));
+
+  for (let i = 0; i < levels.length; ++i) {
+    const level = new Level(levels[i]);
 
     for (const cell of level.edgeCells) {
-      if (cell.type === CellType.block && randBool(0.7)) {
+      if (cell.type === CellType.block) {
         level.removeCell(cell);
       }
     }
 
-    solvable[i] = level.serialize();
+    levels[i] = level.serialize();
+    randomTransformLevel(levels[i]);
   }
 
-  return solvable;
+  return levels;
 };
