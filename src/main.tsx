85b9cbfbf5a0e10ecb8287f2aa3b8f2a1f23ca5c
nilscox <nilscox.dev@gmail.com>
2023-05-05 13:19:16 +0200

    add manifest

--- src/main.tsx
+++ src/main.tsx
@@ -5,3 +5,7 @@ import { App } from './app';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js');
+}
