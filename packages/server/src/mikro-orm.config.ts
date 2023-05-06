51048a86364d54e8b10cf961f617e5fd930de571
nilscox <nilscox.dev@gmail.com>
2023-05-06 20:18:10 +0200

    disable db debug by default

--- packages/server/src/mikro-orm.config.ts
+++ packages/server/src/mikro-orm.config.ts
@@ -3,14 +3,14 @@ import { defineConfig } from '@mikro-orm/sqlite';
 import { SqlLevel } from './entities/level';
 import { SqlLevelSession } from './entities/level-session';
 
-const { DB_PATH: dbPath = './db.sqlite' } = process.env;
+const { DB_PATH: dbPath = './db.sqlite', DB_DEBUG } = process.env;
 
 export default defineConfig({
   type: 'sqlite',
   entities: [SqlLevel, SqlLevelSession],
   dbName: dbPath,
   highlighter: new SqlHighlighter(),
-  debug: true,
+  debug: DB_DEBUG === 'true',
   migrations: {
     path: './src/migrations',
     snapshot: false,
