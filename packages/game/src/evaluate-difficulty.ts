b011d9de1e2763b3023e165402dada8dcf6ac782
nilscox <nilscox.dev@gmail.com>
2023-05-08 15:06:44 +0200

    improve level difficulty evaluation

--- packages/game/src/evaluate-difficulty.ts
+++ packages/game/src/evaluate-difficulty.ts
@@ -1,28 +1,10 @@
-import { CellType, Level, LevelDefinition } from './level';
+import { Level, LevelDefinition } from './level';
 import { Player } from './player';
-import { solve } from './solve';
-import { Direction, Path, getDirectionVector } from './utils/direction';
-import { abs, min } from './utils/math';
+import { Direction, Path, directions } from './utils/direction';
+import { abs, max, min, round } from './utils/math';
 
-export const computeLevelsDifficulties = (levels: Array<Level | LevelDefinition>) => {
-  const difficulties = new Map(
-    levels.map((level, i) => {
-      const difficulty = evaluateLevelDifficulty(level);
-
-      console.log(`${i} / ${levels.length} (${Math.floor((100 * i) / levels.length)}%)`);
-
-      return [level, difficulty];
-    })
-  );
-
-  return (level: Level | LevelDefinition) => {
-    return difficulties.get(level)?.difficulty ?? -1;
-  };
-};
-
-export const evaluateLevelDifficulty = (input: Level | LevelDefinition, solutionsInput?: Path[]) => {
+export const evaluateLevelDifficulty = (input: Level | LevelDefinition, solutions: Path[]) => {
   const level = input instanceof Level ? input : new Level(input);
-  const solutions = solutionsInput ?? solve(level);
 
   if (!solutions || solutions.length === 0) {
     return { difficulty: Infinity };
@@ -32,11 +14,13 @@ export const evaluateLevelDifficulty = (input: Level | LevelDefinition, solution
     solutions.map((solution) => [solution, evaluateSolutionComplexity(level, solution)])
   );
 
-  const numberOfSolutionsScore = Math.floor(Math.max(0, 7 - Math.log2(solutions.length)));
-  const easiestSolutionScore = min(Array.from(solutionsComplexities.values()));
+  const nbCells = level.definition.width * level.definition.height;
+  const numberOfSolutionsScore = round(max([0, 7 - Math.log2(solutions.length)]), 2);
+  const easiestSolutionScore = round(min(Array.from(solutionsComplexities.values())) / Math.sqrt(nbCells), 2);
+  const difficulty = round(numberOfSolutionsScore + easiestSolutionScore, 2);
 
   return {
-    difficulty: numberOfSolutionsScore + easiestSolutionScore,
+    difficulty,
     numberOfSolutions: solutions.length,
     numberOfSolutionsScore,
     easiestSolutionScore,
@@ -46,20 +30,29 @@ export const evaluateLevelDifficulty = (input: Level | LevelDefinition, solution
 
 export const evaluateSolutionComplexity = (level: Level, solution: Direction[]) => {
   const player = new Player(level.start);
-  let prevDir: Direction | undefined;
   let difficulty = 0;
 
-  for (const dir of solution) {
-    if (prevDir && dir !== prevDir) {
-      const [dx, dy] = getDirectionVector(prevDir);
-      const { x, y } = player.position;
+  for (let i = 0; i < solution.length; ++i) {
+    const prevDir = solution[i - 1];
+    const dir = solution[i];
 
-      if (level.atUnsafe(x + dx, y + dy) === CellType.empty) {
-        difficulty += 4;
+    if (prevDir && dir !== prevDir) {
+      if (level.movePlayer(player, prevDir)) {
+        level.movePlayerBack(player);
+        difficulty += 5;
       }
     }
 
-    prevDir = dir;
+    for (const d of directions) {
+      if (d === dir) {
+        continue;
+      }
+
+      if (level.movePlayer(player, d)) {
+        level.movePlayerBack(player);
+        difficulty += 1;
+      }
+    }
 
     const last = player.position;
 
@@ -67,9 +60,7 @@ export const evaluateSolutionComplexity = (level: Level, solution: Direction[]) 
 
     const [dx, dy] = [abs(last.x - player.position.x), abs(last.y - player.position.y)];
 
-    if (dx + dy > 2) {
-      difficulty += 2;
-    }
+    difficulty += dx + dy - 1;
   }
 
   level.restart();
