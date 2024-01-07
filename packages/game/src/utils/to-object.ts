bb78540fd1c457e579322786cbacc992321f473e
nilscox <nils@koyeb.com>
2024-01-07 16:08:11 +0100

    improve toObject typings

--- packages/game/src/utils/to-object.ts
+++ packages/game/src/utils/to-object.ts
@@ -1,13 +1,13 @@
-export const toObject = <T, V>(
+export const toObject = <T, K extends PropertyKey, V>(
   array: T[],
-  getKey: (item: T) => string,
+  getKey: (item: T) => K,
   getValue: (item: T) => V
-): Record<string, V> => {
+): Record<K, V> => {
   return array.reduce(
     (obj, item) => ({
       ...obj,
       [getKey(item)]: getValue(item),
     }),
-    {}
+    {} as Record<K, V>
   );
 };
